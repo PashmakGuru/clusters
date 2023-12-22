e57f6c4d5af5cd3eb9f22f4c390eabff03959314
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2023-12-23 01:43:31 +0330

    Add test-prod

---
+++ prod/entity-test-prod.tf
@@ -0,0 +1,9 @@
+# Port Generated (demo)
+module "kubernetes-cluster" {
+  source  = "app.terraform.io/PashmakGuru/kubernetes-cluster/azure"
+  version = "0.0.1-alpha.1"
+  
+  environment = "prod"
+  location = "West Europe"
+  name = "test"
+}

b7e7a5feb642035618d3d58811cf56bfd601634d
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2023-12-23 02:36:56 +0330

    Update module version

--- prod/entity-test-prod.tf
+++ prod/entity-test-prod.tf
@@ -1,7 +1,7 @@
 # Port Generated (demo)
 module "kubernetes-cluster" {
   source  = "app.terraform.io/PashmakGuru/kubernetes-cluster/azure"
-  version = "0.0.1-alpha.1"
+  version = "0.0.1-alpha.2"
   
   environment = "prod"
   location = "West Europe"

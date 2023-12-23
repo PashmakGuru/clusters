67da1966796f5893e5434af610c8ab39064687aa
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2023-12-23 16:55:35 +0330

    Delete the entity

--- prod/entity-test-prod.tf
+++ prod/entity-test-prod.tf
@@ -1,9 +1,9 @@
 # Port Generated (demo)
-module "kubernetes-cluster" {
-  source  = "app.terraform.io/PashmakGuru/kubernetes-cluster/azure"
-  version = "0.0.1-alpha.2"
+# module "kubernetes-cluster" {
+#   source  = "app.terraform.io/PashmakGuru/kubernetes-cluster/azure"
+#   version = "0.0.1-alpha.2"
   
-  environment = "prod"
-  location = "West Europe"
-  name = "test"
-}
+#   environment = "prod"
+#   location = "West Europe"
+#   name = "test"
+# }

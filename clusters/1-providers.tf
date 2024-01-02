68ac0e07f69b133accb7797f228ca726fa350e4a
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2024-01-02 19:54:32 +0330

    Authenricate azapi

--- clusters/1-providers.tf
+++ clusters/1-providers.tf
@@ -38,6 +38,13 @@ provider "azurerm" {
   tenant_id = var.azure_tenant_id
 }
 
+provider "azapi" {
+  subscription_id = var.azure_subscription_id
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+  tenant_id = var.azure_tenant_id
+}
+
 provider "azuread" {
   client_id     = var.azure_client_id
   client_secret = var.azure_client_secret
